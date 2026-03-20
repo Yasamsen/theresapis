643a81a8aacaf9833d71f70fc3be85ceb6d34f7a
Yasamsen <yasampreset@gmail.com>
2026-03-20 08:50:52 +0800

    replace

---
+++ src/api/Cloudflare/addsub.js
@@ -0,0 +1,99 @@
+const axios = require("axios");
+
+module.exports = function (app) {
+
+  app.get("/cloudflare/addsub", async (req, res) => {
+    const { ip, sub, proxy = "off" } = req.query;
+
+    if (!ip || !sub) {
+      return res.status(400).json({
+        status: false,
+        error: "ip dan sub wajib diisi",
+      });
+    }
+
+    const proxied = proxy === "on";
+    const domain = `${sub}`.'yasamdev.web.id';
+    const zone = '45c900cd8ce4bf53b319e268d241d856';
+
+    try {
+
+      const check = await axios.get(
+        `https://api.cloudflare.com/client/v4/zones/${zone}/dns_records?name=${domain}`,
+        {
+          headers: {
+            Authorization: `Bearer ${global.cloudflare.apiToken}`
+          }
+        }
+      );
+
+      if (check.data.result.length > 0) {
+
+        // update proxy
+        const record = check.data.result[0];
+
+        await axios.patch(
+          `https://api.cloudflare.com/client/v4/zones/${zone}/dns_records/${record.id}`,
+          {
+            proxied: proxied
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${global.cloudflare.apiToken}`,
+              "Content-Type": "application/json"
+            }
+          }
+        );
+
+        return res.status(200).json({
+          status: true,
+          result: {
+            domain,
+            proxied,
+            action: "updated"
+          }
+        });
+
+      } else {
+
+        // create baru
+        await axios.post(
+          `https://api.cloudflare.com/client/v4/zones/${zone}/dns_records`,
+          {
+            type: "A",
+            name: sub,
+            content: ip,
+            ttl: 1,
+            proxied: proxied
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${global.cloudflare.apiToken}`,
+              "Content-Type": "application/json"
+            }
+          }
+        );
+
+        return res.status(200).json({
+          status: true,
+          result: {
+            domain,
+            ip,
+            proxied,
+            action: "created"
+          }
+        });
+
+      }
+
+    } catch (err) {
+      console.log(err.response?.data || err.message);
+      res.status(500).json({
+        status: false,
+        error: "Cloudflare API error"
+      });
+    }
+
+  });
+
+};
